5754540a39c0822968d4c60ba87660b0b695ef18
Tim <timtsai805@gmail.com>
2026-03-30 14:17:17 +0800

    Re-init

--- app/db.py
+++ app/db.py
@@ -9,20 +9,12 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-POSTGRES_URL = os.getenv("POSTGRES_URL")
-SQLITE_URL = os.getenv("SQLITE_URL")
 
 # Get database url
-DATABASE_URL = SQLITE_URL
+POSTGRES_URL = os.getenv("POSTGRES_URL")
 
 # Create DB instance
-if DATABASE_URL.startswith("sqlite"):
-    engine = create_engine(
-        SQLITE_URL,
-        connect_args={"check_same_thread": False}
-    )
-else:
-    engine = create_engine(POSTGRES_URL)
+engine = create_engine(POSTGRES_URL)
 
 
 # Create DB session
--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import auth_router, user_router, device_router, location_router
 
@@ -11,6 +12,15 @@ app = FastAPI(
     title="GNSS_WiFi_Tracker_Open",
     description="This is a RESTful API built with FastAPI",
     version="1.0.0"
+)
+
+# Create CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 # Create all table
